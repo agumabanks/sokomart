f2bcaa998afbd1fefdbf62824995af456bfdca47
ai_Banks <agumabanksibrahim@gmail.com>
2022-05-31 11:10:15 +0300

    seco commit

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/w', function () {
     return view('app');
+});
+
+
+Route::get('/', function () {
+    return view('test');
 });
 
 Route::any('{all}', function($uri) {
